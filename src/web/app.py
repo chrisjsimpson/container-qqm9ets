b202b50fef647fe405ece07b8ccf940f3237ffd6
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-12-05 21:37:41 +0000

    save ChatGPT chats

--- src/web/app.py
+++ src/web/app.py
@@ -1,10 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
+from datetime import datetime
+import os
+import json
+from pathlib import Path
 from db import get_db, close_db
 import sqlalchemy
 from logger import log
 
+
 app = Flask(__name__)
 app.teardown_appcontext(close_db)
+cors = CORS(app, send_wildcard=True)
+CHATS_DIRECTORY = app.config.get("CHATS_DIRECTORY", "./chats/")
+
+
+@app.route("/save", methods=["POST"])
+def save():
+    body = json.loads(request.data)["body"]
+    filename = int(datetime.now().timestamp())
+    with open(f"{CHATS_DIRECTORY}{filename}", "w") as fp:
+        fp.write(body)
+    return jsonify({"msg": f"wrote file: {filename}"})
+
+
+def parse(filename):
+    # Parse the HTML using BeautifulSoup
+    path = Path("./", filename).absolute()
+    with open(path, "r") as fp:
+        text = fp.read()
+        return text
+
+
+@app.route("/chat/<timestamp>")
+def show_chat(timestamp):
+    path = Path(CHATS_DIRECTORY, timestamp).absolute()
+    return parse(path)
+
+
+@app.route("/parse")
+def parse_latest():
+    # Get a list of all the files in the directory
+    files = os.listdir(CHATS_DIRECTORY)
+    # Initialize a variable to keep track of the most recently modified file
+    most_recent_file = None
+    # Iterate over the list of files
+    for file in files:
+        # Get the full path of the current file
+        file_path = os.path.join(CHATS_DIRECTORY, file)
+
+        if most_recent_file is None or os.path.getmtime(
+            file_path
+        ) > os.path.getmtime(  # noqa: E501
+            most_recent_file
+        ):
+            # If the current file is more recently modified, store
+            # its path as the
+            # most recently modified file
+            most_recent_file = file_path
+    parsed = parse(most_recent_file)
+    return render_template("parsed.html", parsed=parsed)
 
 
 @app.route("/")
